d2d7d1d46d148fde78b3afbf2571bd38fd031a0c
Yash Garg <yashygkf@gmail.com>
2023-08-16 17:59:09 +0530

    test case: burn function

--- test/DemoswapPairV2Test.t.sol
+++ test/DemoswapPairV2Test.t.sol
@@ -13,7 +13,11 @@ contract DemoswapPairV2Test is Test {
 
     DemoswapPairV2 public pair;
 
+    TestUser public testUser;
+
     function setUp() public {
+        testUser = new TestUser();
+
         token0 = new ERC20Mintable("TokenA", "TA");
         token1  = new ERC20Mintable("TokenB", "TB");
 
@@ -67,11 +71,94 @@ contract DemoswapPairV2Test is Test {
         assertReserves(3 ether, 2 ether);
     }
 
+    function testBurn() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // mint 1 LP token
+
+        pair.burn();
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1000, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1000);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 1000);
+    }
+
+    function testBurnUnbalanced() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // mint 1 LP token
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // mint 1 LP token
+
+        pair.burn();
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1500, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1500);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 1000);
+    }
+
+    function testBurnUnbalancedDifferentUsers() public {
+        testUser.provideLiquidity(
+            address(pair),
+            address(token0),
+            address(token1),
+            1 ether,
+            1 ether
+        ); //mint 1 LP token for test user
+
+        assertEq(pair.balanceOf(address(this)),0);
+        assertEq(pair.balanceOf(address(testUser)), 1 ether - 1000);
+        assertEq(pair.totalSupply(), 1 ether);
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // mint 1 LP token for this contract
+
+        assertEq(pair.balanceOf(address(this)), 1 ether); // not -1000 because it didn't initialize the liquidity
+
+        pair.burn();
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1.5 ether, 1 ether);
+        assertEq(pair.totalSupply(), 1 ether);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 0.5 ether);
+        assertEq(token1.balanceOf(address(this)), 10 ether);
+    }
+
     function assertReserves(uint112 expectedReserve0, uint112 expectedReserve1)
         internal
     {
         (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
         assertEq(reserve0, expectedReserve0, "unexpected reserve0");
         assertEq(reserve1, expectedReserve1, "unexpected reserve1");
+    }
+}
+
+contract TestUser {
+    function provideLiquidity(
+        address pairAddress_,
+        address token0Address_,
+        address token1Address_,
+        uint256 amount0_,
+        uint256 amount1_
+    ) public {
+        ERC20(token0Address_).transfer(pairAddress_, amount0_);
+        ERC20(token1Address_).transfer(pairAddress_, amount1_);
+
+        ZuniswapV2Pair(pairAddress_).mint();
+    }
+
+    function withdrawLiquidity(address pairAddress_) public {
+        ZuniswapV2Pair(pairAddress_).burn();
     }
 }
